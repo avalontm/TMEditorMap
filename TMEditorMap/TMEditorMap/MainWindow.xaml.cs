754c0a60355e036cf6a6ea3a5392fad1a01c1548
Λv∂lΘи ™ <jaimeraulmendezlopez325@hotmail.com>
2021-05-09 01:18:58 -0700

    Iniciando Proyecto

---
+++ TMEditorMap/TMEditorMap/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TMEditorMap
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        void onLoaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onUnloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onNew(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onOpen(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onSave(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        void onExit(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
